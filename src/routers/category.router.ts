cd98897675e2a4304a97f5aa286e196e4a5d89b5
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-09-01 18:19:39 +0300

    rename: categoriesRouter -> categoryRouter

--- src/routers/category.router.ts
+++ src/routers/category.router.ts
@@ -1,35 +1,35 @@
 import express from "express";
 
-const categoriesRouter = express.Router();
+const categoryRouter = express.Router();
 
-categoriesRouter.get("/", (req, res) => {
+categoryRouter.get("/", (req, res) => {
   res.json({ message: "All categories" });
 });
 
-categoriesRouter.get("/:category_id", (req, res) => {
+categoryRouter.get("/:category_id", (req, res) => {
   const { category_id } = req.params;
   res.json({ message: `Category ${category_id}` });
 });
 
-categoriesRouter.get("/:category_id/posts", (req, res) => {
+categoryRouter.get("/:category_id/posts", (req, res) => {
   const { category_id } = req.params;
   res.json({ message: `Posts for category ${category_id}` });
 });
 
-categoriesRouter.post("/", (req, res) => {
+categoryRouter.post("/", (req, res) => {
   const { title } = req.body;
   res.json({ message: `Category created`, title });
 });
 
-categoriesRouter.patch("/:category_id", (req, res) => {
+categoryRouter.patch("/:category_id", (req, res) => {
   const { category_id } = req.params;
   const { title } = req.body;
   res.json({ message: `Category ${category_id} updated`, title });
 });
 
-categoriesRouter.delete("/:category_id", (req, res) => {
+categoryRouter.delete("/:category_id", (req, res) => {
   const { category_id } = req.params;
   res.json({ message: `Category ${category_id} deleted` });
 });
 
-export { categoriesRouter };
+export { categoryRouter };
