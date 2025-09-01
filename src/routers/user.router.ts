9f8af1dfbfa41980b2e38a049f5058695dc8d3f1
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-09-01 18:03:21 +0300

    feat: implement user router with CRUD operations

---
+++ src/routers/user.router.ts
@@ -0,0 +1,36 @@
+import express from "express";
+
+const userRouter = express.Router();
+
+userRouter.get("/", (req, res) => {
+  res.send("User list");
+});
+
+userRouter.get("/:user_id", (req, res) => {
+  const { user_id } = req.params;
+  res.send(`User details for user ID: ${user_id}`);
+});
+
+// ADMINS ONLY
+userRouter.post("/", (req, res) => {
+  if (1) {
+    throw new Error("Only admins can create users");
+  }
+  res.status(201).send("User created");
+});
+
+userRouter.patch("/avatar", (req, res) => {
+  res.send(`Profile picture updated`);
+});
+
+userRouter.patch("/:user_id", (req, res) => {
+  const { user_id } = req.params;
+  res.send(`User details updated for user ID: ${user_id}`);
+});
+
+userRouter.delete("/:user_id", (req, res) => {
+  const { user_id } = req.params;
+  res.send(`User deleted for user ID: ${user_id}`);
+});
+
+export { userRouter };
