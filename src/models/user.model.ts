8340ea612820dcb70c23bd7b4a63c33566d1aacf
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-09-01 18:37:46 +0300

    fix: rename password field to password_hash and add password_salt in user model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -3,7 +3,8 @@ export type IUserRole = "user" | "admin";
 export type IUserModel = {
   id: number;
   login: string;
-  password: string;
+  password_hash: string;
+  password_salt: string;
   full_name: string;
   email: string;
   avatar: string;
@@ -15,7 +16,8 @@ export class UserModel implements IUserModel {
   constructor(
     public id: number,
     public login: string,
-    public password: string,
+    public password_hash: string,
+    public password_salt: string,
     public full_name: string,
     public email: string,
     public avatar: string,
