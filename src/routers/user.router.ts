d2370ec31277f8426a67c982b056c98b1c1ee760
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-09-01 18:13:53 +0300

    feat: enhance user creation and update routes with request body parameter extraction

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -16,6 +16,7 @@ userRouter.post("/", (req, res) => {
   if (1) {
     throw new Error("Only admins can create users");
   }
+  const { login, password, passwordConfirmation, email, role } = req.body;
   res.status(201).send("User created");
 });
 
@@ -25,6 +26,7 @@ userRouter.patch("/avatar", (req, res) => {
 
 userRouter.patch("/:user_id", (req, res) => {
   const { user_id } = req.params;
+  // Partial<UserModel>
   res.send(`User details updated for user ID: ${user_id}`);
 });
 
