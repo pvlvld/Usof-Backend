26e71986c79f53c93042a10069976e21fb58c701
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-09-02 23:35:25 +0300

    feat: add DTO classes for user authentication and password management

---
+++ src/dto/auth.dto.ts
@@ -0,0 +1,66 @@
+import {
+  IsEmail,
+  IsString,
+  MinLength,
+  MaxLength,
+  IsOptional
+} from "class-validator";
+
+export class RegisterDto {
+  @IsString()
+  @MinLength(6)
+  @MaxLength(50)
+  login!: string;
+
+  @IsString()
+  @MinLength(6)
+  @MaxLength(100)
+  password!: string;
+
+  @IsString()
+  @MinLength(6)
+  @MaxLength(100)
+  passwordConfirmation!: string;
+
+  @IsEmail()
+  email!: string;
+}
+
+export class LoginDto {
+  @IsOptional()
+  @IsString()
+  login?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsString()
+  @MinLength(6)
+  password!: string;
+}
+
+export class PasswordResetRequestDto {
+  @IsEmail()
+  email!: string;
+}
+
+export class PasswordResetDto {
+  @IsString()
+  confirm_token!: string;
+
+  @IsString()
+  @MinLength(6)
+  password!: string;
+
+  @IsString()
+  @MinLength(6)
+  passwordConfirmation!: string;
+}
+
+export class LogoutDTO {
+  @IsString()
+  @MinLength(6)
+  @MaxLength(100)
+  refreshToken!: string;
+}
