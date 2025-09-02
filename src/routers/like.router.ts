737929327fb0d61ba3bba6d7acc08e0c60617b46
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-09-02 19:18:10 +0300

    feat: update like router to handle dynamic post and comment IDs

--- src/routers/like.router.ts
+++ src/routers/like.router.ts
@@ -2,9 +2,29 @@ import express from "express";
 
 const likeRouter = express.Router();
 
-likeRouter.post("/like", (req, res) => {
+likeRouter.post("/:id/like", (req, res) => {
   // Like | Dislike | Unset (Null?)
-  res.status(200).send({ message: "Success!" });
+  const { id } = req.params;
+  const { action } = req.body;
+
+  let targetType = "";
+  if (req.baseUrl.includes("/posts")) {
+    targetType = "post";
+  } else if (req.baseUrl.includes("/comments")) {
+    targetType = "comment";
+  } else {
+    throw new Error("Like action must be associated with a post or comment");
+  }
+
+  if (action === "like") {
+    //
+  } else if (action === "dislike") {
+    //
+  } else if (action === "unset") {
+    //
+  }
+
+  res.status(200).send({ message: `Success!` });
 });
 
 export { likeRouter };
