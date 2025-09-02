32a4bc7bda1a0e75240a741cd9d3d3cc7840c7d1
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-09-02 19:12:07 +0300

    feat: integrate like router into post and comment routers

--- src/routers/api.router.ts
+++ src/routers/api.router.ts
@@ -4,8 +4,12 @@ import { userRouter } from "./user.router.js";
 import { postRouter } from "./post.router.js";
 import { commentRouter } from "./comment.router.js";
 import { categoryRouter } from "./category.router.js";
+import { likeRouter } from "./like.router.js";
 
 const apiRouter = express.Router();
+
+postRouter.use("/like", likeRouter);
+commentRouter.use("/like", likeRouter);
 
 apiRouter.use("/auth", authRouter);
 apiRouter.use("/users", userRouter);
