c64e2652b1c258063f2652f3103c8c2251401a9e
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-09-02 19:08:52 +0300

    feat: implement initial Express server setup with API routing

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import { apiRouter } from "./routers/api.router.js";
+
+const app = express();
+
+// MIDDLEWARES
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.redirect("/api");
+});
+
+app.use("/api", apiRouter);
+
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
