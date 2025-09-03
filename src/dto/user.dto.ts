df7a5f52221e462eff64f89711cbca6c53206f10
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-09-03 17:58:11 +0300

    fix: add validation decorators to user DTO classes for improved data integrity

--- src/dto/user.dto.ts
+++ src/dto/user.dto.ts
@@ -1,5 +1,7 @@
 import {
+  IsEmail,
   IsNumber,
+  IsOptional,
   IsString,
   Max,
   MaxLength,
@@ -16,4 +18,82 @@ export class GetUsersDto {
   @Min(1)
   @Max(100)
   limit!: number;
+}
+
+export class GetUserByIdDTO {
+  @IsNumber()
+  @Min(1)
+  user_id!: number;
+}
+
+export class CreateUserDTO {
+  @IsString()
+  @MinLength(2)
+  @MaxLength(100)
+  login!: string;
+
+  @IsString()
+  @MinLength(6)
+  @MaxLength(100)
+  password!: string;
+
+  @IsString()
+  @MinLength(6)
+  @MaxLength(100)
+  passwordConfirmation!: string;
+
+  @IsString()
+  @IsEmail()
+  email!: string;
+
+  @IsString()
+  @MaxLength(50)
+  role!: string;
+}
+
+export class UploadUserAvatarDTO {
+  @IsString()
+  @MaxLength(100)
+  user_id!: string;
+
+  @IsString()
+  @MaxLength(100)
+  avatar!: string;
+}
+
+export class UpdateUserDataDTO {
+  @IsString()
+  @MaxLength(100)
+  user_id!: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(100)
+  login?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(100)
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(50)
+  role?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(100)
+  avatar?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(100)
+  fullName?: string;
+}
+
+export class DeleteUserDTO {
+  @IsNumber()
+  @Min(1)
+  user_id!: number;
 }
