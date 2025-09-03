0c26fcaa5f00df7a1d42db9396b282ec0cbafd9e
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-09-03 17:42:18 +0300

    fix: add cookie-parser middleware for cookie handling

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { apiRouter } from "./routers/api.router.js";
-
+import cookieParser from "cookie-parser";
 const app = express();
 
 // MIDDLEWARES
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.redirect("/api");
