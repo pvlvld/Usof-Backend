fda46d3289db06501da76558544942b2809c10c0
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-09-07 16:52:54 +0300

    fix: add isCustomError property to CustomError class

--- src/consts/errors.ts
+++ src/consts/errors.ts
@@ -1,5 +1,6 @@
 export class CustomError extends Error {
   statusCode: number;
+  isCustomError: true = true;
   constructor(message: string, statusCode: number) {
     super(message);
     this.statusCode = statusCode;
