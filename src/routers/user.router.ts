b6600792eb074c1485dac762b155dabb28a086d5
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-09-07 21:57:03 +0300

    feat: implement avatar upload functionality with multer and add file type validation

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -4,12 +4,44 @@ import express, {
   type Response
 } from "express";
 import { userController } from "../controllers/user.controller.js";
+import multer from "multer";
+import path from "node:path";
 import {
   authenticateMiddleware,
   requireAdminMiddleware
 } from "../middlewares/auth.middleware.js";
 
 const userRouter = express.Router();
+
+// TODO:
+// - Utilize auth data to get user_id
+// - Move to the controller
+// - Convert all to the webp
+// - Async i/o? If possible
+const uploadAvatar = multer({
+  dest: "public/uploads/avatars/",
+  limits: { fileSize: 5 * 1024 * 1024, files: 1 },
+  fileFilter: (req, file, cb) => {
+    const allowedMimeTypes = ["image/jpeg", "image/png", "image/webp"];
+    if (allowedMimeTypes.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new Error("Invalid file type. Only JPEG, PNG and WEBP are allowed."));
+    }
+  },
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "public/uploads/avatars/");
+    },
+    filename: (req, file, cb) => {
+      const ext = path.extname(file.originalname);
+      console.log(file);
+      console.log(req.body);
+      const filename = `avatar_${req.body.user_id}${ext}`;
+      cb(null, filename);
+    }
+  })
+});
 
 userRouter.get("/", (req: Request, res: Response, next: NextFunction) => {
   userController.getUsers(req, res, next);
@@ -33,8 +65,20 @@ userRouter.post(
 );
 
 userRouter.patch(
+  "/avatar",
+  authenticateMiddleware,
+  uploadAvatar.single("avatar")
+);
+
+// TODO: how in da hell create regex route?
+// https://expressjs.com/en/guide/routing.html
+// https://github.com/pillarjs/path-to-regexp
+// https://bjohansebas.github.io/playground-router/
+userRouter.patch(
   "/:user_id",
+  authenticateMiddleware,
   (req: Request, res: Response, next: NextFunction) => {
+    console.log("Update user");
     userController.updateUser(req, res, next);
   }
 );
