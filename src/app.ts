20e855829b7832f034148706e61d9e3b8d1f360b
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-09-08 17:58:34 +0300

    refactor: move app initialization logic to createApp function

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import express, { type Request, type Response } from "express";
+import cookieParser from "cookie-parser";
+import { apiRouter } from "./api/api.router.js";
+import { initializeAdminJs } from "./admin.js";
+import {
+  authenticateMiddleware,
+  requireAdminMiddleware
+} from "./shared/middlewares/auth.middleware.js";
+import { errorHandler } from "./shared/middlewares/errorHandler.middleware.js";
+
+export async function createApp() {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cookieParser());
+
+  const adminRouter = await initializeAdminJs();
+  app.use(
+    "/admin",
+    authenticateMiddleware,
+    requireAdminMiddleware,
+    adminRouter
+  );
+
+  app.get("/", (req: Request, res: Response) => {
+    res.redirect("/api");
+  });
+
+  app.use("/api", apiRouter);
+
+  app.use(errorHandler);
+
+  return app;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,10 @@
 import "reflect-metadata";
-import express, { type Request, type Response } from "express";
-import { apiRouter } from "./api/api.router.js";
-import cookieParser from "cookie-parser";
-import { errorHandler } from "./shared/middlewares/errorHandler.middleware.js";
-import { initializeAdminJs } from "./admin.js";
-import {
-  authenticateMiddleware,
-  requireAdminMiddleware
-} from "./shared/middlewares/auth.middleware.js";
+import { createApp } from "./app.js";
 
 async function start() {
-  const app = express();
-
-  app.use(express.json());
-  app.use(cookieParser());
-
-  const adminRouter = await initializeAdminJs();
-  app.use(
-    "/admin",
-    authenticateMiddleware,
-    requireAdminMiddleware,
-    adminRouter
-  );
-
-  app.get("/", (req: Request, res: Response) => {
-    res.redirect("/api");
-  });
-
-  app.use("/api", apiRouter);
-
-  app.use(errorHandler);
-
+  const app = await createApp();
   const PORT = process.env.PORT ? +process.env.PORT : 3000;
+
   app.listen(PORT, () => {
     console.log(
       `Server is running on http://localhost:${PORT} in ${
