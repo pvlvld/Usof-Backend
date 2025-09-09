4f7bf62bef74b62ec99bb4ddce899a574a91bf67
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-09-09 17:32:41 +0300

    feat: add middleware for avatar upload with file type validation and storage configuration

---
+++ src/shared/middlewares/uploadAvatar.middleware.ts
@@ -0,0 +1,32 @@
+import multer from "multer";
+import { InternalServerError } from "../consts/errors.js";
+import path from "node:path";
+
+const uploadAvatar = multer({
+  dest: "public/uploads/avatars/",
+  limits: { fileSize: 5 * 1024 * 1024, files: 1 },
+  fileFilter: (req, file, cb) => {
+    const allowedMimeTypes = ["image/jpeg", "image/png", "image/webp"];
+    if (allowedMimeTypes.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(
+        new InternalServerError(
+          "Invalid file type. Only JPEG, PNG and WEBP are allowed."
+        )
+      );
+    }
+  },
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "public/uploads/avatars/");
+    },
+    filename: (req, file, cb) => {
+      const ext = path.extname(file.originalname);
+      const filename = `${req.user?.id}${ext}`;
+      cb(null, filename);
+    }
+  })
+});
+
+export { uploadAvatar };
