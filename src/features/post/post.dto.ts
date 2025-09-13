9f1bccf83d021ee70dddac097ea119c7194eade4
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-09-13 19:20:43 +0300

    fix: update categories type in CreatePostDTO from string[] to number[] for consistency

--- src/features/post/post.dto.ts
+++ src/features/post/post.dto.ts
@@ -32,10 +32,8 @@ export class CreatePostDTO extends UserIdDTO {
   @IsString()
   content!: string;
 
-  @MinLength(1, { each: true })
-  @MaxLength(20, { each: true })
-  @IsString({ each: true })
-  categories!: string[];
+  @IsInt({ each: true })
+  categories!: number[];
 }
 
 export class PostLikeDislikeDTO extends PostIdDTO {
