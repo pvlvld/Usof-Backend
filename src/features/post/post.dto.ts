294f86a157118872af304f503f751877c87915d7
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-09-13 19:09:44 +0300

    feat: add DTOs for post management including creation, commenting, and liking/disliking

---
+++ src/features/post/post.dto.ts
@@ -0,0 +1,52 @@
+import {
+  IsInt,
+  IsOptional,
+  IsString,
+  MaxLength,
+  Min,
+  MinLength
+} from "class-validator";
+
+export class PostIdDTO {
+  @Min(1)
+  @IsInt()
+  post_id!: number;
+}
+
+export class CreatePostCommentDTO extends PostIdDTO {
+  @MinLength(1)
+  @MaxLength(500)
+  @IsString()
+  content!: string;
+}
+
+export class CreatePostDTO {
+  @MinLength(1)
+  @MaxLength(200)
+  @IsString()
+  title!: string;
+
+  @MinLength(1)
+  @MaxLength(5000)
+  @IsString()
+  content!: string;
+
+  @MinLength(1, { each: true })
+  @MaxLength(20, { each: true })
+  @IsString({ each: true })
+  categories!: string[];
+}
+
+export class PostLikeDislikeDTO extends PostIdDTO {
+  @IsOptional()
+  dislike?: boolean;
+}
+
+export class PostCommentIdDTO {
+  @Min(1)
+  @IsInt()
+  comment_id!: number;
+}
+
+/** Post creator only */
+export class PostUpdateDTO extends CreatePostDTO {}
