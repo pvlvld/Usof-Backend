099e260070ec225504fa6e496fff750a1ea09000
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-09-14 17:43:25 +0300

    feat: implement LikeModel with CRUD operations for likes and dislikes

---
+++ src/features/like/like.model.ts
@@ -0,0 +1,56 @@
+import type { RowDataPacket } from "mysql2";
+import { QUERIES } from "../../shared/consts/queries.js";
+import Database from "../../shared/database/index.js";
+
+export class LikeModel {
+  private static instance: LikeModel;
+  private db: ReturnType<typeof Database.getPool>;
+
+  private constructor() {
+    this.db = Database.getPool();
+  }
+
+  public static getInstance(): LikeModel {
+    if (!this.instance) {
+      this.instance = new LikeModel();
+    }
+    return this.instance;
+  }
+
+  public async findOne(
+    userId: number,
+    postId: number | null,
+    commentId: number | null
+  ): Promise<{ is_like: boolean } | null> {
+    const [rows] = await this.db.query<RowDataPacket[]>(QUERIES.LIKE.READ, [
+      userId,
+      postId,
+      commentId
+    ]);
+    if (!Array.isArray(rows)) return null;
+    console.log(rows);
+    return rows[0] ? { is_like: !!rows[0]?.is_like } : null;
+  }
+
+  public async upsert(
+    userId: number,
+    postId: number | null,
+    commentId: number | null,
+    isLike: boolean
+  ): Promise<any> {
+    return this.db.query(QUERIES.LIKE.UPSERT, [
+      userId,
+      postId,
+      commentId,
+      isLike
+    ]);
+  }
+
+  public async delete(
+    userId: number,
+    postId: number | null,
+    commentId: number | null
+  ): Promise<any> {
+    return this.db.query(QUERIES.LIKE.DELETE, [userId, postId, commentId]);
+  }
+}
