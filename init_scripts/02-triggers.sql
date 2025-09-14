20a475faeb47841275d329d2b65955333e9500fb
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-09-14 18:00:53 +0300

    fix: correct rating update logic in after_like_dislike_update trigger

--- init_scripts/02-triggers.sql
+++ init_scripts/02-triggers.sql
@@ -41,20 +41,35 @@ BEGIN
     DECLARE target_user_id INT;
     IF OLD.post_id IS NOT NULL THEN
         SELECT user_id INTO target_user_id FROM post WHERE id = OLD.post_id;
+        IF OLD.is_like != NEW.is_like THEN
+            IF OLD.is_like THEN
+                UPDATE user SET rating = rating - 1 WHERE id = target_user_id;
+                UPDATE post SET rating = rating - 1 WHERE id = OLD.post_id;
+            ELSE
+                UPDATE user SET rating = rating + 1 WHERE id = target_user_id;
+                UPDATE post SET rating = rating + 1 WHERE id = OLD.post_id;
+            END IF;
+            IF NEW.is_like THEN
+                UPDATE user SET rating = rating + 1 WHERE id = target_user_id;
+                UPDATE post SET rating = rating + 1 WHERE id = NEW.post_id;
+            ELSE
+                UPDATE user SET rating = rating - 1 WHERE id = target_user_id;
+                UPDATE post SET rating = rating - 1 WHERE id = NEW.post_id;
+            END IF;
+        END IF;
     ELSEIF OLD.comment_id IS NOT NULL THEN
         SELECT user_id INTO target_user_id FROM comment WHERE id = OLD.comment_id;
-    END IF;
-    IF OLD.is_like != NEW.is_like THEN
-        IF OLD.is_like THEN
-            UPDATE user SET rating = rating - 1 WHERE id = target_user_id;
-        ELSE
-            UPDATE user SET rating = rating + 1 WHERE id = target_user_id;
-        END IF;
-
-        IF NEW.is_like THEN
-            UPDATE user SET rating = rating + 1 WHERE id = target_user_id;
-        ELSE
-            UPDATE user SET rating = rating - 1 WHERE id = target_user_id;
+        IF OLD.is_like != NEW.is_like THEN
+            IF OLD.is_like THEN
+                UPDATE user SET rating = rating - 1 WHERE id = target_user_id;
+            ELSE
+                UPDATE user SET rating = rating + 1 WHERE id = target_user_id;
+            END IF;
+            IF NEW.is_like THEN
+                UPDATE user SET rating = rating + 1 WHERE id = target_user_id;
+            ELSE
+                UPDATE user SET rating = rating - 1 WHERE id = target_user_id;
+            END IF;
         END IF;
     END IF;
 END//
