0aba6766a3f9e2ea9721dbc241ada4b9463b9310
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-09-19 21:10:53 +0300

    feat: implement CategoryService with CRUD operations for categories

---
+++ src/features/category/category.service.ts
@@ -0,0 +1,59 @@
+import { NotFoundError } from "../../shared/consts/errors.js";
+import type { CreateCategoryDto, UpdateCategoryDto } from "./category.dto.js";
+import { CategoryModel } from "./category.model.js";
+
+class CategoryService {
+  private static instance: CategoryService | null = null;
+  private categoryModel: CategoryModel;
+
+  private constructor(category: typeof CategoryModel) {
+    this.categoryModel = category.getInstance();
+  }
+
+  public static getInstance(category: typeof CategoryModel) {
+    if (!this.instance) {
+      this.instance = new CategoryService(category);
+    }
+    return this.instance;
+  }
+
+  public async getCategories() {
+    return await this.categoryModel.getAllCategories();
+  }
+
+  public async getCategoryById(category_id: number) {
+    return await this.categoryModel.getCategoryById(category_id);
+  }
+
+  public async createCategory(dto: CreateCategoryDto) {
+    return await this.categoryModel.createCategory(dto);
+  }
+
+  public async updateCategory(category_id: number, dto: UpdateCategoryDto) {
+    const category = await this.categoryModel.getCategoryById(category_id);
+
+    if (!category) {
+      throw new NotFoundError("Category not found");
+    }
+
+    Object.keys(dto).forEach((key) => {
+      if (dto[key] === null) {
+        category[key] = null;
+      } else if (dto[key] !== undefined) {
+        category[key] = dto[key];
+      }
+    });
+
+    return await this.categoryModel.updateCategory(category_id, category);
+  }
+
+  public async deleteCategory(category_id: number) {
+    const isDeleted = await this.categoryModel.deleteCategory(category_id);
+    if (!isDeleted) {
+      throw new NotFoundError("Category not found");
+    }
+    return isDeleted;
+  }
+}
+
+export { CategoryService };
