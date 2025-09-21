b5f9a5ad1a160d708fcac69d4ea446bbb4f4a05f
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-09-21 20:35:38 +0300

    feat: add GetPostsDto class with pagination, sorting, and filtering options

--- src/features/post/post.dto.ts
+++ src/features/post/post.dto.ts
@@ -1,8 +1,10 @@
-import { Type } from "class-transformer";
+import { Transform, Type } from "class-transformer";
 import {
+  IsIn,
   IsInt,
   IsOptional,
   IsString,
+  Max,
   MaxLength,
   Min,
   MinLength
@@ -13,6 +15,56 @@ export class PostIdDTO {
   @Type(() => Number)
   @IsInt()
   post_id!: number;
+}
+
+export class GetPostsDto {
+  @Min(1)
+  @Type(() => Number)
+  @IsInt()
+  @IsOptional()
+  page: number = 1;
+
+  @Min(1)
+  @Max(100)
+  @Type(() => Number)
+  @IsInt()
+  @IsOptional()
+  limit: number = 25;
+
+  // Don't allow created_at sorting, map it to id to avoid creating additional indexes
+  @Transform(({ value }) => (value === "created_at" ? "id" : value))
+  @IsIn(["id", "login", "rating", "created_at", "updated_at"], {
+    message:
+      "sort must be one of the following values: id, login, rating, created_at, updated_at"
+  })
+  @Transform(({ value }) => ("" + value).toLowerCase())
+  @IsString()
+  @IsOptional()
+  // Stupid default, but ok, it's a requirement
+  sort: "id" | "login" | "rating" | "created_at" | "updated_at" = "rating";
+
+  @IsIn(["ASC", "DESC"], {
+    message: "order must be either 'ASC' or 'DESC'"
+  })
+  @Transform(({ value }) => ("" + value).toUpperCase())
+  @IsString()
+  @IsOptional()
+  order: "ASC" | "DESC" = "ASC";
+
+  @Transform(({ value }) => ("" + value).toLowerCase())
+  @IsString()
+  @IsOptional()
+  categories: string = "all";
+
+  @IsString()
+  @IsOptional()
+  status: "active" | "inactive" = "active";
+
+  @Min(0)
+  @Type(() => Number)
+  @IsInt()
+  @IsOptional()
+  user: number = 0;
 }
 
 export class CreatePostCommentDTO extends PostIdDTO {
