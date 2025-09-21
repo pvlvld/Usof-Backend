4ca9a592623a6bbff501ca5256bc0e03976e99c1
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-09-21 19:28:22 +0300

    feat: enhance error handling for UnsafeQueryError with user info logging

--- src/shared/middlewares/errorHandler.middleware.ts
+++ src/shared/middlewares/errorHandler.middleware.ts
@@ -1,5 +1,6 @@
 import type { Request, Response, NextFunction } from "express";
-import { CustomError } from "../consts/errors.js";
+import { CustomError, UnsafeQueryError } from "../consts/errors.js";
+import { authenticateMiddleware } from "./auth.middleware.js";
 
 export function errorHandler(
   err: unknown,
@@ -8,7 +9,22 @@ export function errorHandler(
   next: NextFunction
 ) {
   if (err instanceof CustomError) {
-    res.status(err.statusCode).json({ error: err.message });
+    if (err instanceof UnsafeQueryError) {
+      authenticateMiddleware(req, res, () => {}, true);
+      const userInfo = <Record<string, unknown>>(req.user ?? {});
+      userInfo["ip"] = req.ip;
+      userInfo["user-agent"] = req.headers["user-agent"];
+
+      console.error(
+        "[GlobalErrorHandler] Unsafe query detected:",
+        err._query,
+        `Request info: ${req.method} ${req.originalUrl}`,
+        `User info: ${JSON.stringify(userInfo)}`
+      );
+      res.status(err.statusCode).json({ error: "Bad Request" });
+    } else {
+      res.status(err.statusCode).json({ error: err.message });
+    }
   } else {
     console.error("[GlobalErrorHandler] Unexpected error:", err);
     res.status(500).json({
