b365d0a345a00d65e2012e91889285631d322b9b
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-09-22 22:36:11 +0300

    feat: install urlencoded middleware

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,7 @@ export async function createApp() {
   const app = express();
 
   app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
   app.use(cookieParser());
 
   const adminRouter = await initializeAdminJs();
