9c322a57417f73f6ecdd9bd321fe119f3d8b8ed9
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-09-25 18:20:29 +0300

    feat: add DTO classes for CommentId, CreateComment, and UpdateComment

---
+++ src/features/comment/comment.dto.ts
@@ -0,0 +1,28 @@
+import { IsInt, IsOptional, Length, Min, MinLength } from "class-validator";
+import type { ICommentModel } from "./comment.model.js";
+import { Type } from "class-transformer";
+
+export class CommentIdDTO {
+  @Min(1)
+  @Type(() => Number)
+  @IsInt()
+  id!: number;
+}
+
+export class CreateCommentDTO
+  implements Pick<ICommentModel, "parent_id" | "content">
+{
+  @Min(1)
+  @Type(() => Number)
+  @IsInt()
+  @IsOptional()
+  parent_id: number | null = null;
+
+  @Length(1, 5000)
+  content!: string;
+}
+
+export class UpdateCommentDTO {
+  @Length(1, 5000)
+  content!: string;
+}
