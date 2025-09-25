d2557c64ee868f3d0921fb438b7802bfcd64e51a
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-09-25 21:42:35 +0300

    feat: add ecosystem configuration for production deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: "Usof_Backend",
+      script: "dist/index.js",
+      args: "--env-file=.env",
+      env: {
+        NODE_ENV: "production",
+      },
+      instances: 1,
+      exec_mode: "fork",
+      log_date_format: "MM-DD HH:mm:ss",
+      kill_timeout: 60000,
+      no_pmx: true,
+    }
+  ],
+};
