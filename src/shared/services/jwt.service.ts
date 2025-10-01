c4293ced8c966e9db3613ea1158037751f8c7a49
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-10-01 16:59:10 +0300

    refactor: reorganize imports and improve JWT expiration handling

--- src/shared/services/jwt.service.ts
+++ src/shared/services/jwt.service.ts
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
-import type { StringValue } from "ms";
+import ms from "ms";
 
+import type { StringValue } from "ms";
 import type { JwtPayload as JwtPayloadBase } from "jsonwebtoken";
 import type { IUserRole } from "../../features/user/user.dto.js";
 
@@ -11,12 +12,18 @@ type IJwtPayload = JwtPayloadBase & {
 export type IJwtPayloadAuth = IJwtPayload & {
   role: IUserRole;
 };
+
+function StringValueTypeGuard(value: unknown): value is StringValue {
+  if (typeof value === "number") return true;
+  return ms(value as any) !== undefined;
+}
+
 class JwtService {
   private static instance: JwtService | null = null;
   private readonly accessSecret: string;
   private readonly refreshSecret: string;
-  private readonly accessExpiresIn: StringValue;
-  private readonly refreshExpiresIn: StringValue;
+  private readonly accessExpiresIn: StringValue | number;
+  private readonly refreshExpiresIn: StringValue | number;
 
   // Just in case
   private algorithm: jwt.Algorithm = "HS256";
@@ -29,8 +36,12 @@ class JwtService {
     this.accessSecret = process.env.JWT_SECRET || "json_secret";
     this.refreshSecret =
       process.env.JWT_REFRESH_SECRET || "json_refresh_secret";
-    this.accessExpiresIn = "15m";
-    this.refreshExpiresIn = "7d";
+
+    const rawAccessTtl = process.env.JWT_ACCESS_TTL || "15m";
+    this.accessExpiresIn = ms(<StringValue>rawAccessTtl) || ms("15m");
+
+    const rawRefreshTtl = process.env.JWT_REFRESH_TTL || "30d";
+    this.refreshExpiresIn = ms(<StringValue>rawRefreshTtl) || ms("30d");
   }
 
   public static getInstance() {
