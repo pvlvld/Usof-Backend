0cecee3a4da92686f13557dbff6b247a47c8c085
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-10-24 19:24:20 +0300

    feat: add pwnedPasswordAlert template for compromised password notifications

--- src/shared/consts/emailTemplates.ts
+++ src/shared/consts/emailTemplates.ts
@@ -18,6 +18,18 @@ const EMAIL_TEMPLATES = {
       <br>
       <p>If you did not request a password reset, please ignore this email.</p>
     `;
+  },
+  pwnedPasswordAlert: (compromisedCount: number) => {
+    return `
+      <h1>USOF - Compromised Password Alert</h1>
+      <br>
+      <p>Our system has detected that your password has appeared in data breaches ${compromisedCount} times.</p>
+      <p>For more information and to perform further checks, visit Have I Been Pwned:</p>
+      <p><a href="https://haveibeenpwned.com">https://haveibeenpwned.com</a></p>
+      <p>To specifically check passwords, see the Pwned Passwords service:</p>
+      <p><a href="https://haveibeenpwned.com/Passwords">https://haveibeenpwned.com/Passwords</a></p>
+      <p>Please avoid pasting your current password into third‑party sites if you are unsure. Instead, change your password to a strong, unique one and enable multi-factor authentication.</p>
+    `;
   }
 };
 
