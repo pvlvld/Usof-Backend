e31390390038e5aabd96a8c128fee35d06b2a2b2
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-10-24 22:05:28 +0300

    fix: update ecosystem.config.mjs to use ES module syntax

--- ecosystem.config.mjs
+++ ecosystem.config.mjs
@@ -1,17 +1,17 @@
-module.exports = {
+export default {
   apps: [
     {
       name: "Usof_Backend",
       script: "dist/index.js",
       args: "--env-file=.env",
       env: {
-        NODE_ENV: "production",
+        NODE_ENV: "production"
       },
       instances: 1,
       exec_mode: "fork",
       log_date_format: "MM-DD HH:mm:ss",
       kill_timeout: 60000,
-      no_pmx: true,
+      no_pmx: true
     }
-  ],
+  ]
 };
