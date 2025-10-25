0a3968bc6057f2799ba51d3216f16335e824dd08
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-10-25 06:46:14 +0300

    refactor: extract sanitization logic into a separate method for improved readability

--- src/shared/middlewares/sanitizePostOrComment.middleware.ts
+++ src/shared/middlewares/sanitizePostOrComment.middleware.ts
@@ -13,53 +13,57 @@ class SanitizePostOrCommentMiddlewareBuilder {
   getMiddleware() {
     return (req: Request, res: Response, next: NextFunction) => {
       if (req.body.content && typeof req.body.content === "string") {
-        req.body.content = this.domPurify.sanitize(req.body.content, {
-          ALLOWED_TAGS: [
-            "p",
-            "br",
-            "strong",
-            "b",
-            "em",
-            "i",
-            "u",
-            "a",
-            "ul",
-            "ol",
-            "li",
-            "blockquote",
-            "h1",
-            "h2",
-            "h3",
-            "h4",
-            "h5",
-            "h6",
-            "img",
-            "table",
-            "thead",
-            "tbody",
-            "tr",
-            "th",
-            "td",
-            "span"
-          ],
-          ALLOWED_ATTR: [
-            "href",
-            "target",
-            "rel",
-            "src",
-            "alt",
-            "title",
-            "width",
-            "height",
-            "style",
-            "align",
-            "colspan",
-            "rowspan"
-          ]
-        });
+        req.body.content = this.sanitizer(req.body.content);
       }
       return next();
     };
+  }
+
+  sanitizer(content: string) {
+    return this.domPurify.sanitize(content, {
+      ALLOWED_TAGS: [
+        "p",
+        "br",
+        "strong",
+        "b",
+        "em",
+        "i",
+        "u",
+        "a",
+        "ul",
+        "ol",
+        "li",
+        "blockquote",
+        "h1",
+        "h2",
+        "h3",
+        "h4",
+        "h5",
+        "h6",
+        "img",
+        "table",
+        "thead",
+        "tbody",
+        "tr",
+        "th",
+        "td",
+        "span"
+      ],
+      ALLOWED_ATTR: [
+        "href",
+        "target",
+        "rel",
+        "src",
+        "alt",
+        "title",
+        "width",
+        "height",
+        "style",
+        "align",
+        "colspan",
+        "rowspan"
+      ]
+    });
   }
 }
 
