a19b60a4839b7babf3a0267db4bfb68e89b761bd
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-10-25 06:04:39 +0300

    feat: add randomInArray utility function

---
+++ src/shared/utils/randomInArray.ts
@@ -0,0 +1,7 @@
+export function randomInArray<T>(arr: T[]): T | undefined {
+  if (arr.length === 0) {
+    return undefined;
+  }
+  const randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex] as T;
+}
